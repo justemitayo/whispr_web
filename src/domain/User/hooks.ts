b77daec05b883895cc8e35ed527cfc14252fa29b
justemitayo <temitayoakindeju@gmail.com>
2025-05-14 21:03:31 +0100

    fix: hook.ts

--- src/domain/User/hooks.ts
+++ src/domain/User/hooks.ts
@@ -1,26 +1,27 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { getUsers } from './api';
-import { GetUsersRequest } from './types';
+import { GetUsersRequest, GetUsersResponse } from './types';
 
 export const useGetUsers = (payload: GetUsersRequest) => {
-  return useInfiniteQuery(
-    ['getUsers', payload.search, payload.limit],
-    ({ pageParam = 1 }) =>
+  return useInfiniteQuery<GetUsersResponse, Error, GetUsersResponse,  [string, string, number?],  number  >({
+  queryKey: ['getUsers', payload.search, payload.limit],
+  initialPageParam: 1,
+    queryFn: ({ pageParam = 1 }) =>
       getUsers({
         search: payload.search,
         page: pageParam,
         limit: payload.limit || 20,
       }),
-    {
-      getNextPageParam: (lastPage, _allPages) => {
+      getNextPageParam: (lastPage: GetUsersResponse) => {
         const nextPage = lastPage?.data?.page?.next_page;
         return nextPage ? nextPage : undefined;
       },
       refetchIntervalInBackground: true,
       refetchOnReconnect: true,
       refetchOnWindowFocus: true,
-    },
-  );
+      enabled: !!payload.search,
+
+});
 }
 
 
