88d1896d660c22761044da7a184770a2bd22e66d
justemitayo <temitayoakindeju@gmail.com>
2025-05-23 17:42:11 +0100

    ui changes

--- src/screen/Messenger/Messenger.tsx
+++ src/screen/Messenger/Messenger.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Messenger.css'
 import Conversation from '../../Components/Cnversation/Conversation'
 import Message from '../../Components/Message/Message'
+import user from '../../assets/images/default_user_dark.jpg'
+import { Online } from '../../Components/Online/Online'
 
 const Messenger = () => {
   return (
@@ -34,6 +36,18 @@ const Messenger = () => {
       </div>
       <div className='chat-box'>
         <div className='box-wrapper'>
+          <div className='chat-header'>
+            <div className='chat-header-top'>
+              <div style={{position:'relative'}}>
+                <img alt='' src={user} style={{width:'3.5rem', height:'3.5rem', borderRadius:"50%"}} />
+                <Online />
+              </div>
+              <div style={{display:'flex', flexDirection:"column"}}>
+                <span>John Doe</span>
+                <span>heyy</span>
+              </div> 
+            </div>
+          </div>
           <div className='box-top'>
             <Message own={false}/>
             <Message own={true}/>
@@ -42,29 +56,19 @@ const Messenger = () => {
             <Message own={false} />
             <Message own={false} />
             <Message own={false} />
+            <Message own={true}/>
+            <Message own={false} />
+            <Message own={false} />
+            <Message own={false} />
+            <Message own={false} />
+            <Message own={true}/>
             <Message own={false} />
             <Message own={false} />
             <Message own={false} />
             <Message own={false} />
             <Message own={false} />
             <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
-            <Message own={false} />
+            <Message own={true}/>
             <Message own={false} />
             <Message own={false} />
             <Message own={false} />
