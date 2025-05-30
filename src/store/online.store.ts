c0ef1bb2df403fae1bf82f30b3375d06eb1af0a7
justemitayo <temitayoakindeju@gmail.com>
2025-05-30 15:07:24 +0100

    online-store state management

---
+++ src/store/online.store.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware';
+import { IOnlineUser } from "../interface/socket";
+
+type State = {
+  online_user: IOnlineUser[]
+}
+
+type Action = {
+  updateOnlineUsers: (online_users: IOnlineUser[]) => void;
+  isOnline: (user_id: IOnlineUser['user_id']) => boolean;
+  clearOnlineUsers: () => void;
+}
+
+export const useChatStore = create<
+  State & Action,
+  [['zustand/persist', unknown]] 
+>(
+  persist(
+    (set, get) => ({
+      online_user: [],
+      updateOnlineUsers: online_user => set(() => ({ online_user })),
+      isOnline: user_id => {
+        return get().online_user.some(user => user.user_id === user_id);
+      },
+      clearOnlineUsers: () => set(() => ({ online_user: [] })),
+    }),
+    {
+      name: 'online-users-storage',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+)
